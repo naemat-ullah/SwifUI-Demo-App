c32862c40020a493cb06be7f5538dd13df613a10
Naemat Ullah <naematullah.dev@gmail.com>
2024-11-17 17:14:59 +0500

    initial commit

---
+++ SwiftUI-Demo-App/SwiftUI_Demo_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Demo_AppApp.swift
+//  SwiftUI-Demo-App
+//
+//  Created by Naemat Ullah on 17/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Demo_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
